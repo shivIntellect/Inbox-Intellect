6ddf68b2fd6c723b4ef2fb301e9bec77912b2980
shivIntellect <shivsundar21@outlook.com>
2024-01-20 00:54:49 +0530

    Initial commit

---
+++ src/main/java/com/inboxintellect/InboxIntellectApplication.java
@@ -0,0 +1,13 @@
+package com.inboxintellect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxIntellectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxIntellectApplication.class, args);
+	}
+
+}
